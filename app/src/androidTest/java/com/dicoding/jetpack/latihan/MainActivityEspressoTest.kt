ba84ce53461073c8486e7340c525a7dd73755ca9
Wilham <wilham182@gmail.com>
2019-09-09 11:08:00 +0700

    fix eror unit test and instrument test

--- app/src/androidTest/java/com/dicoding/jetpack/latihan/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/com/dicoding/jetpack/latihan/MainActivityEspressoTest.kt
@@ -4,24 +4,25 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityEspressoTest {
-    val dummyVolume = "504.0"
-    val dummyCircumference = "2016.0"
-    val dummySurfaceArea = "396.0"
-    val dummyLength = "12.0"
-    val dummyWidth = "7.0"
-    val dummyHeight = "6.0"
-    val emptyInput = ""
-    val fieldEmpty = "Field ini tidak boleh kosong"
+    //val dummyVolume = "504.0"
+    private val dummyCircumference = "2016.0"
+    //val dummySurfaceArea = "396.0"
+    private val dummyLength = "12.0"
+    private val dummyWidth = "7.0"
+    private val dummyHeight = "6.0"
+    //val emptyInput = ""
+    //val fieldEmpty = "Field ini tidak boleh kosong"
 
     @Rule
+    @JvmField
     val mActivityRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
